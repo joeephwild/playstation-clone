dea28eb9a36f69af55dcee714d7dda33c711b704
joeephwild <josephomotade0@gmail.com>
2022-08-26 20:21:46 +0000

    commit

---
+++ utils/HeroData.ts
@@ -0,0 +1,35 @@
+import hero from '../public/hero-banner.webp'
+import hero2 from '../public/hero-banner2.webp'
+import hero3 from '../public/hero-banner3.webp'
+import hero4 from '../public/hero-banner4.webp'
+import hero5 from '../public/hero-banner5.webp'
+
+
+export const Herodata = [
+   {
+    id: 1,
+    image: hero,
+    thumbnail: hero,
+   },
+   {
+    id: 2,
+    image: hero2,
+    thumbnail: hero2,
+   },
+   {
+    id: 3,
+    image: hero3,
+    thumbnail: hero3,
+   },
+   {
+    id: 4,
+    image: hero4,
+    thumbnail: hero4,
+   },
+
+   {
+    id: 5,
+    image: hero5,
+    thumbnail: hero5,
+   },
+]
